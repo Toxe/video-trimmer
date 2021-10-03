53b0fbd750614a0228af4b66d4c1276520279634
Toxe <toxe@toxe.net>
2021-10-03 13:18:20 +0200

    Delete trace message

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -1,9 +1,6 @@
 #include "app.h"
 
-#include <spdlog/spdlog.h>
-
 void App::next_frame()
 {
     elapsed_time_ = frame_time_clock_.restart();
-    spdlog::trace("elapsed time: {}s ({} FPS)", elapsed_time_.as_seconds(), elapsed_time_.fps());
 }
