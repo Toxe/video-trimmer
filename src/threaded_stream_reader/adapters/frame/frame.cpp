e30f70148b983a81307bed001fa59bff5a4ce09a
Toxe <toxe@toxe.net>
2021-11-09 08:39:39 +0100

    Only scale down, never scale frames up to be bigger than the source frame

--- src/threaded_stream_reader/adapters/frame/frame.cpp
+++ src/threaded_stream_reader/adapters/frame/frame.cpp
@@ -7,8 +7,14 @@ Frame::Frame(CodecContext* codec_context, const int scaled_width, const int scal
     src_width_ = codec_context->width();
     src_height_ = codec_context->height();
 
-    dst_width_ = scaled_width;
-    dst_height_ = scaled_height;
+    // only scale down, never scale frames up to be bigger than the source frame
+    if (scaled_width <= src_width_ && scaled_height <= src_height_) {
+        dst_width_ = scaled_width;
+        dst_height_ = scaled_height;
+    } else {
+        dst_width_ = src_width_;
+        dst_height_ = src_height_;
+    }
 }
 
 double Frame::timestamp() const
