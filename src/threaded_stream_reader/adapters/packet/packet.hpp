bfdba0aed74aec7954d428985a6208f7b0556de7
Toxe <toxe@toxe.net>
2021-11-15 18:26:46 +0100

    Name parameter

--- src/threaded_stream_reader/adapters/packet/packet.hpp
+++ src/threaded_stream_reader/adapters/packet/packet.hpp
@@ -7,7 +7,7 @@ public:
     virtual ~Packet() = default;
 
     [[nodiscard]] virtual int stream_index() const = 0;
-    virtual void set_stream_index(const int) { }
+    virtual void set_stream_index(const int /* new_stream_index */) { }
 
     [[nodiscard]] virtual AVPacket* packet() const = 0;
     virtual void unref() = 0;
