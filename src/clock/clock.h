bc4636af11aac1cd0a202a9aba2d392b870ece73
Toxe <toxe@toxe.net>
2021-11-15 16:58:24 +0100

    clock_ is not needed

--- src/clock/clock.h
+++ src/clock/clock.h
@@ -5,20 +5,20 @@
 #include "duration.h"
 
 class Clock {
-    std::chrono::steady_clock clock_;
     std::chrono::steady_clock::time_point start_;
 
 public:
-    Clock() : start_{clock_.now()} {}
+    Clock() : start_{std::chrono::steady_clock::now()} { }
 
-    Duration elapsed_time() {
-        auto now = clock_.now();
+    Duration elapsed_time()
+    {
+        auto now = std::chrono::steady_clock::now();
         return Duration{now - start_};
     }
 
     Duration restart()
     {
-        auto now = clock_.now();
+        auto now = std::chrono::steady_clock::now();
         auto dur = now - start_;
         start_ = now;
         return Duration{dur};
