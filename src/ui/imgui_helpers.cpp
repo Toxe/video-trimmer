0eb83542b01952c4bce3800c3c7374937d37ee1d
Toxe <toxe@toxe.net>
2021-11-18 08:35:59 +0100

    Add ImGui helper function to draw outlined text

---
+++ src/ui/imgui_helpers.cpp
@@ -0,0 +1,18 @@
+#include "imgui_helpers.hpp"
+
+// Draw outlined text.
+void imgui_text_outlined(const ImVec4& text_color, const ImVec4& outline_color, const std::string& text)
+{
+    ImDrawList* draw_list = ImGui::GetWindowDrawList();
+    const ImVec2 pos = ImGui::GetCursorScreenPos();
+    const float outline_size = 1.0f;
+
+    for (int x = -1; x <= 1; ++x)
+        for (int y = -1; y <= 1; ++y)
+            if (x != 0 && y != 0)
+                draw_list->AddText(ImVec2(pos.x + static_cast<float>(x) * outline_size, pos.y + static_cast<float>(y) * outline_size), ImGui::GetColorU32(outline_color), text.c_str());
+
+    draw_list->AddText(pos, ImGui::GetColorU32(text_color), text.c_str());
+
+    ImGui::NewLine();
+}
---
+++ src/ui/imgui_helpers.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+#include <imgui.h>
+
+void imgui_text_outlined(const ImVec4& text_color, const ImVec4& outline_color, const std::string& text);
