ab2abb92e19edb77f61cd79029f789d76720b677
Toxe <toxe@toxe.net>
2021-11-27 08:21:33 +0100

    only show info for available streams

--- src/ui/widgets/video_file_info_widget.cpp
+++ src/ui/widgets/video_file_info_widget.cpp
@@ -14,11 +14,15 @@ void VideoFileInfoWidget::render(const VideoFile* video_file)
     ImGui::NewLine();
     ImGui::TextColored(UserInterface::Colors::light_blue, fmt::format("{} ({})", video_file->filename(), video_file->file_format()).c_str());
 
-    ImGui::TextColored(UserInterface::Colors::light_blue, fmt::format("stream #{} ({}):", video_file->video_stream_info()->stream_index(), video_file->video_stream_info()->codec_type()).c_str());
-    ImGui::TextColored(UserInterface::Colors::light_blue, fmt::format("    {}", video_file->video_stream_info()->codec_name()).c_str());
-    ImGui::TextColored(UserInterface::Colors::light_blue, fmt::format("    {}", video_file->video_stream_info()->codec_additional_info()).c_str());
+    if (video_file->has_video_stream()) {
+        ImGui::TextColored(UserInterface::Colors::light_blue, fmt::format("stream #{} ({}):", video_file->video_stream_info()->stream_index(), video_file->video_stream_info()->codec_type()).c_str());
+        ImGui::TextColored(UserInterface::Colors::light_blue, fmt::format("    {}", video_file->video_stream_info()->codec_name()).c_str());
+        ImGui::TextColored(UserInterface::Colors::light_blue, fmt::format("    {}", video_file->video_stream_info()->codec_additional_info()).c_str());
+    }
 
-    ImGui::TextColored(UserInterface::Colors::light_blue, fmt::format("stream #{} ({}):", video_file->audio_stream_info()->stream_index(), video_file->audio_stream_info()->codec_type()).c_str());
-    ImGui::TextColored(UserInterface::Colors::light_blue, fmt::format("    {}", video_file->audio_stream_info()->codec_name()).c_str());
-    ImGui::TextColored(UserInterface::Colors::light_blue, fmt::format("    {}", video_file->audio_stream_info()->codec_additional_info()).c_str());
+    if (video_file->has_audio_stream()) {
+        ImGui::TextColored(UserInterface::Colors::light_blue, fmt::format("stream #{} ({}):", video_file->audio_stream_info()->stream_index(), video_file->audio_stream_info()->codec_type()).c_str());
+        ImGui::TextColored(UserInterface::Colors::light_blue, fmt::format("    {}", video_file->audio_stream_info()->codec_name()).c_str());
+        ImGui::TextColored(UserInterface::Colors::light_blue, fmt::format("    {}", video_file->audio_stream_info()->codec_additional_info()).c_str());
+    }
 }
