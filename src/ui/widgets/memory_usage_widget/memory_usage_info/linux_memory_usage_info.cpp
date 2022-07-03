9840ea41fddd7972b0280fd3fb5b889f5882c65a
Toxe <toxe@toxe.net>
2022-07-03 14:11:48 +0200

    Add TODOs

--- src/ui/widgets/memory_usage_widget/memory_usage_info/linux_memory_usage_info.cpp
+++ src/ui/widgets/memory_usage_widget/memory_usage_info/linux_memory_usage_info.cpp
@@ -1,5 +1,6 @@
 #include "linux_memory_usage_info.hpp"
 
+// TODO: implement for Linux
 float LinuxMemoryUsageInfo::get_memory_usage()
 {
     return to_mbyte(0.0f);
--- src/ui/widgets/memory_usage_widget/memory_usage_info/mac_memory_usage_info.cpp
+++ src/ui/widgets/memory_usage_widget/memory_usage_info/mac_memory_usage_info.cpp
@@ -1,5 +1,6 @@
 #include "mac_memory_usage_info.hpp"
 
+// TODO: implement for Mac
 float MacMemoryUsageInfo::get_memory_usage()
 {
     return to_mbyte(0.0f);
