70ae849aac107d30e3cffd533c9c001c53e5aa5c
Toxe <toxe@toxe.net>
2022-07-17 09:55:58 +0200

    Clean up event handling

--- src/video_trimmer/event_handler/event_handler.cpp
+++ src/video_trimmer/event_handler/event_handler.cpp
@@ -11,6 +11,25 @@ namespace video_trimmer::event_handler {
 
 const Command no_command = [] { video_trimmer::logger::log_debug("NoCommand"); };
 
+void handle_window_events(EventHandler& event_handler, const SDL_Event& event)
+{
+    if (event.window.event == SDL_WINDOWEVENT_RESIZED)
+        event_handler.handle_event(Event::ResizedWindow);
+    else if (event.window.event == SDL_WINDOWEVENT_CLOSE)
+        event_handler.handle_event(Event::CloseWindow);
+}
+
+void handle_keyboard_events(EventHandler& event_handler, const SDL_Event& event)
+{
+    if (ImGui::GetIO().WantCaptureKeyboard)
+        return;
+
+    if (event.key.keysym.sym == SDLK_ESCAPE)
+        event_handler.handle_event(Event::CloseWindow);
+    else if (event.key.keysym.sym == SDLK_F1)
+        event_handler.handle_event(Event::ToggleHelp);
+}
+
 EventHandler::EventHandler()
 {
     commands_[Event::CloseWindow] = no_command;
@@ -37,20 +56,10 @@ void EventHandler::poll_events()
 
         if (event.type == SDL_QUIT)
             handle_event(Event::CloseWindow);
-        else if (event.type == SDL_WINDOWEVENT) {
-            if (event.window.event == SDL_WINDOWEVENT_RESIZED)
-                handle_event(Event::ResizedWindow);
-            else if (event.window.event == SDL_WINDOWEVENT_CLOSE)
-                handle_event(Event::CloseWindow);
-        } else if (event.type == SDL_KEYDOWN) {
-            if (ImGui::GetIO().WantCaptureKeyboard)
-                continue;
-
-            if (event.key.keysym.sym == SDLK_ESCAPE)
-                handle_event(Event::CloseWindow);
-            else if (event.key.keysym.sym == SDLK_F1)
-                handle_event(Event::ToggleHelp);
-        }
+        else if (event.type == SDL_WINDOWEVENT)
+            handle_window_events(*this, event);
+        else if (event.type == SDL_KEYDOWN)
+            handle_keyboard_events(*this, event);
     }
 }
 
