3d2858c9cb5c4b06799d59e0a5c089139df803e9
Toxe <toxe@toxe.net>
2022-07-21 14:10:27 +0200

    Make pimpl

--- src/video_trimmer/event_handler/event_handler.cpp
+++ src/video_trimmer/event_handler/event_handler.cpp
@@ -1,5 +1,7 @@
 #include "event_handler.hpp"
 
+#include <unordered_map>
+
 #include "SDL2/SDL.h"
 #include "imgui.h"
 #include "imgui_impl_sdl.h"
@@ -11,45 +13,42 @@ namespace video_trimmer::event_handler {
 
 const Command no_command = [] { video_trimmer::logger::log_debug("NoCommand"); };
 
-void handle_window_events(EventHandler& event_handler, const SDL_Event& event)
-{
-    if (event.window.event == SDL_WINDOWEVENT_RESIZED)
-        event_handler.handle_event(Event::ResizedWindow);
-    else if (event.window.event == SDL_WINDOWEVENT_CLOSE)
-        event_handler.handle_event(Event::CloseWindow);
-}
+class EventHandler::Impl {
+public:
+    Impl();
 
-void handle_keyboard_events(EventHandler& event_handler, const SDL_Event& event)
-{
-    if (ImGui::GetIO().WantCaptureKeyboard)
-        return;
+    void set_command(const Event& event, Command command);
 
-    if (event.key.keysym.sym == SDLK_ESCAPE)
-        event_handler.handle_event(Event::CloseWindow);
-    else if (event.key.keysym.sym == SDLK_F1)
-        event_handler.handle_event(Event::ToggleHelp);
-    else if (event.key.keysym.sym == SDLK_SPACE)
-        event_handler.handle_event(Event::PlaybackTogglePause);
-}
+    void poll_events();
+    void handle_event(const Event& event);
 
-EventHandler::EventHandler()
+private:
+    std::unordered_map<Event, Command> commands_;
+
+    void handle_window_events(const SDL_Event& event);
+    void handle_keyboard_events(const SDL_Event& event);
+};
+
+EventHandler::Impl::Impl()
 {
     commands_[Event::CloseWindow] = no_command;
     commands_[Event::ResizedWindow] = no_command;
     commands_[Event::ToggleHelp] = no_command;
+    commands_[Event::PlaybackStart] = no_command;
+    commands_[Event::PlaybackTogglePause] = no_command;
 }
 
-void EventHandler::set_command(const Event& event, Command command)
+void EventHandler::Impl::set_command(const Event& event, Command command)
 {
     commands_[event] = command;
 }
 
-void EventHandler::handle_event(const Event& event)
+void EventHandler::Impl::handle_event(const Event& event)
 {
     commands_[event]();
 }
 
-void EventHandler::poll_events()
+void EventHandler::Impl::poll_events()
 {
     SDL_Event event;
 
@@ -59,10 +58,37 @@ void EventHandler::poll_events()
         if (event.type == SDL_QUIT)
             handle_event(Event::CloseWindow);
         else if (event.type == SDL_WINDOWEVENT)
-            handle_window_events(*this, event);
+            handle_window_events(event);
         else if (event.type == SDL_KEYDOWN)
-            handle_keyboard_events(*this, event);
+            handle_keyboard_events(event);
     }
 }
+
+void EventHandler::Impl::handle_window_events(const SDL_Event& event)
+{
+    if (event.window.event == SDL_WINDOWEVENT_RESIZED)
+        handle_event(Event::ResizedWindow);
+    else if (event.window.event == SDL_WINDOWEVENT_CLOSE)
+        handle_event(Event::CloseWindow);
+}
+
+void EventHandler::Impl::handle_keyboard_events(const SDL_Event& event)
+{
+    if (ImGui::GetIO().WantCaptureKeyboard)
+        return;
+
+    if (event.key.keysym.sym == SDLK_ESCAPE)
+        handle_event(Event::CloseWindow);
+    else if (event.key.keysym.sym == SDLK_F1)
+        handle_event(Event::ToggleHelp);
+    else if (event.key.keysym.sym == SDLK_SPACE)
+        handle_event(Event::PlaybackTogglePause);
+}
+
+EventHandler::EventHandler() : impl_(std::make_unique<EventHandler::Impl>()) { }
+EventHandler::~EventHandler() = default;
+void EventHandler::set_command(const Event& event, Command command) { impl_->set_command(event, command); }
+void EventHandler::poll_events() { impl_->poll_events(); }
+void EventHandler::handle_event(const Event& event) { impl_->handle_event(event); }
 
 }  // namespace video_trimmer::event_handler
--- src/video_trimmer/event_handler/event_handler.hpp
+++ src/video_trimmer/event_handler/event_handler.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <unordered_map>
+#include <memory>
 
 #include "command.hpp"
 #include "events.hpp"
@@ -10,14 +10,16 @@ namespace video_trimmer::event_handler {
 class EventHandler {
 public:
     EventHandler();
+    ~EventHandler();
 
-    void set_command(const Event& key_event, Command command);
+    void set_command(const Event& event, Command command);
 
     void poll_events();
     void handle_event(const Event& event);
 
 private:
-    std::unordered_map<Event, Command> commands_;
+    class Impl;
+    std::unique_ptr<Impl> impl_;
 };
 
 }  // namespace video_trimmer::event_handler
