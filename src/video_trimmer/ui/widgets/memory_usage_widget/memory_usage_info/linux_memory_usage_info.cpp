458c7ce5ab5a27d7141d032aae7a2960368681c4
Toxe <toxe@toxe.net>
2022-08-08 10:45:46 +0200

    Add memory usage info for Linux

--- src/video_trimmer/ui/widgets/memory_usage_widget/memory_usage_info/linux_memory_usage_info.cpp
+++ src/video_trimmer/ui/widgets/memory_usage_widget/memory_usage_info/linux_memory_usage_info.cpp
@@ -1,11 +1,29 @@
 #include "linux_memory_usage_info.hpp"
 
+#include <fstream>
+
+#include <unistd.h>
+
+#include "video_trimmer/error/error.hpp"
+
 namespace video_trimmer::ui::widgets::memory_usage_widget::memory_usage_info {
 
-// TODO: implement for Linux
+LinuxMemoryUsageInfo::LinuxMemoryUsageInfo()
+{
+    page_size_ = static_cast<int>(sysconf(_SC_PAGESIZE));
+}
+
 float LinuxMemoryUsageInfo::get_memory_usage()
 {
-    return to_mbyte(0.0f);
+    std::ifstream input{"/proc/self/statm"};
+
+    if (!input.is_open())
+        error::die("unable to open file: /proc/self/statm");
+
+    int size, resident;
+    input >> size >> resident;
+
+    return to_mbyte(static_cast<float>(resident * page_size_));
 }
 
 }  // namespace video_trimmer::ui::widgets::memory_usage_widget::memory_usage_info
--- src/video_trimmer/ui/widgets/memory_usage_widget/memory_usage_info/linux_memory_usage_info.hpp
+++ src/video_trimmer/ui/widgets/memory_usage_widget/memory_usage_info/linux_memory_usage_info.hpp
@@ -6,7 +6,12 @@ namespace video_trimmer::ui::widgets::memory_usage_widget::memory_usage_info {
 
 class LinuxMemoryUsageInfo : public MemoryUsageInfo {
 public:
+    LinuxMemoryUsageInfo();
+
     [[nodiscard]] float get_memory_usage() override;
+
+private:
+    int page_size_;
 };
 
 }  // namespace video_trimmer::ui::widgets::memory_usage_widget::memory_usage_info
