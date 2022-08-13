301e74f3c5b67bd7b2c252b2608e1c92dfcc5eec
Toxe <toxe@toxe.net>
2022-08-13 09:28:15 +0200

    Add default values

--- src/video_trimmer/include/types.hpp
+++ src/video_trimmer/include/types.hpp
@@ -3,11 +3,13 @@
 namespace video_trimmer {
 
 struct Position {
-    int x, y;
+    int x = 0;
+    int y = 0;
 };
 
 struct Size {
-    int width, height;
+    int width = 0;
+    int height = 0;
 };
 
 inline bool operator==(const Size& lhs, const Size& rhs) { return lhs.width == rhs.width && lhs.height == rhs.height; }
