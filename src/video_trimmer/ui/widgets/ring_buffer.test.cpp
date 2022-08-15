7441d8e46fdb87ab36be4cd3cfb711e5fc4c5c3d
Toxe <toxe@toxe.net>
2022-08-15 15:04:10 +0200

    Use CHECK and Catch::Approx()

--- src/video_trimmer/ui/widgets/ring_buffer.test.cpp
+++ src/video_trimmer/ui/widgets/ring_buffer.test.cpp
@@ -1,3 +1,4 @@
+#include "catch2/catch_approx.hpp"
 #include "catch2/catch_test_macros.hpp"
 #include "catch2/matchers/catch_matchers_floating_point.hpp"
 
@@ -10,19 +11,19 @@ TEST_CASE("ui::widgets::RingBuffer")
     SECTION("Can create RingBuffers with different sizes")
     {
         RingBuffer buffer1(1);
-        REQUIRE(buffer1.size() == 1);
+        CHECK(buffer1.size() == 1);
 
         RingBuffer buffer2(10);
-        REQUIRE(buffer2.size() == 10);
+        CHECK(buffer2.size() == 10);
 
         RingBuffer buffer3(100);
-        REQUIRE(buffer3.size() == 100);
+        CHECK(buffer3.size() == 100);
     }
 
     SECTION("New RingBuffers need to have a size of at least 1")
     {
-        REQUIRE_THROWS(RingBuffer(0));
-        REQUIRE_THROWS(RingBuffer(-1));
+        CHECK_THROWS(RingBuffer(0));
+        CHECK_THROWS(RingBuffer(-1));
     }
 
     SECTION("Can push and retrieve values")
@@ -30,13 +31,13 @@ TEST_CASE("ui::widgets::RingBuffer")
         RingBuffer buffer(10);
 
         buffer.push(1.0f);
-        REQUIRE(buffer.get(0) == 1.0f);
+        CHECK(buffer.get(0) == Catch::Approx(1.0f));
 
         buffer.push(2.0f);
-        REQUIRE(buffer.get(1) == 2.0f);
+        CHECK(buffer.get(1) == Catch::Approx(2.0f));
 
         buffer.push(3.0f);
-        REQUIRE(buffer.get(2) == 3.0f);
+        CHECK(buffer.get(2) == Catch::Approx(3.0f));
     }
 
     SECTION("All values in a new RingBuffer are 0.0 by default")
@@ -44,20 +45,20 @@ TEST_CASE("ui::widgets::RingBuffer")
         RingBuffer buffer(5);
 
         for (int i = 0; i < buffer.size(); ++i)
-            REQUIRE(buffer.get(i) == 0.0f);
+            CHECK(buffer.get(i) == Catch::Approx(0.0f));
     }
 
     SECTION("The offset points to the next insertion position")
     {
         RingBuffer buffer(10);
 
-        REQUIRE(buffer.offset() == 0);
+        CHECK(buffer.offset() == 0);
 
         buffer.push(1.0f);
-        REQUIRE(buffer.offset() == 1);
+        CHECK(buffer.offset() == 1);
 
         buffer.push(1.0f);
-        REQUIRE(buffer.offset() == 2);
+        CHECK(buffer.offset() == 2);
     }
 
     SECTION("Once the offset reaches the end it wraps around to the first element")
@@ -66,11 +67,11 @@ TEST_CASE("ui::widgets::RingBuffer")
 
         buffer.push(1.0f);
         buffer.push(2.0f);
-        REQUIRE(buffer.offset() == 2);
+        CHECK(buffer.offset() == 2);
         buffer.push(3.0f);
-        REQUIRE(buffer.offset() == 0);
+        CHECK(buffer.offset() == 0);
         buffer.push(4.0f);
-        REQUIRE(buffer.offset() == 1);
+        CHECK(buffer.offset() == 1);
     }
 
     SECTION("Can retrieve the last element")
@@ -81,7 +82,7 @@ TEST_CASE("ui::widgets::RingBuffer")
             const auto f = static_cast<float>(i);
 
             buffer.push(f);
-            REQUIRE(buffer.last() == f);
+            CHECK(buffer.last() == Catch::Approx(f));
         }
     }
 
@@ -89,29 +90,29 @@ TEST_CASE("ui::widgets::RingBuffer")
     {
         RingBuffer buffer(10);
 
-        REQUIRE(buffer.data() != nullptr);
+        CHECK(buffer.data() != nullptr);
     }
 
     SECTION("max() returns the biggest element")
     {
         RingBuffer buffer(10);
 
-        REQUIRE(buffer.max() == 0.0f);
+        CHECK(buffer.max() == Catch::Approx(0.0f));
 
         buffer.push(1.0f);
-        REQUIRE(buffer.max() == 1.0f);
+        CHECK(buffer.max() == Catch::Approx(1.0f));
 
         buffer.push(3.0f);
-        REQUIRE(buffer.max() == 3.0f);
+        CHECK(buffer.max() == Catch::Approx(3.0f));
 
         buffer.push(-1.0f);
-        REQUIRE(buffer.max() == 3.0f);
+        CHECK(buffer.max() == Catch::Approx(3.0f));
 
         buffer.push(0.0f);
-        REQUIRE(buffer.max() == 3.0f);
+        CHECK(buffer.max() == Catch::Approx(3.0f));
 
         buffer.push(5.0f);
-        REQUIRE(buffer.max() == 5.0f);
+        CHECK(buffer.max() == Catch::Approx(5.0f));
     }
 }
 
