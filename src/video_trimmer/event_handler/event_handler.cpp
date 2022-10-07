7b18a47c5d3631f5e0ddde89787e9d2b9719be58
Toxe <toxe@toxe.net>
2022-10-07 16:32:47 +0200

    Use switch instead of if/else

--- src/video_trimmer/event_handler/event_handler.cpp
+++ src/video_trimmer/event_handler/event_handler.cpp
@@ -60,21 +60,20 @@ void EventHandler::Impl::poll_events()
     while (SDL_PollEvent(&event)) {
         ImGui_ImplSDL2_ProcessEvent(&event);
 
-        if (event.type == SDL_QUIT)
-            handle_event(Event::CloseWindow);
-        else if (event.type == SDL_WINDOWEVENT)
-            handle_window_events(event);
-        else if (event.type == SDL_KEYDOWN)
-            handle_keyboard_events(event);
+        switch (event.type) {
+            case SDL_QUIT: handle_event(Event::CloseWindow); break;
+            case SDL_WINDOWEVENT: handle_window_events(event); break;
+            case SDL_KEYDOWN: handle_keyboard_events(event); break;
+        }
     }
 }
 
 void EventHandler::Impl::handle_window_events(const SDL_Event& event)
 {
-    if (event.window.event == SDL_WINDOWEVENT_RESIZED)
-        handle_event(Event::ResizedWindow);
-    else if (event.window.event == SDL_WINDOWEVENT_CLOSE)
-        handle_event(Event::CloseWindow);
+    switch (event.window.event) {
+        case SDL_WINDOWEVENT_RESIZED: handle_event(Event::ResizedWindow); break;
+        case SDL_WINDOWEVENT_CLOSE: handle_event(Event::CloseWindow); break;
+    }
 }
 
 void EventHandler::Impl::handle_keyboard_events(const SDL_Event& event)
@@ -82,20 +81,15 @@ void EventHandler::Impl::handle_keyboard_events(const SDL_Event& event)
     if (ImGui::GetIO().WantCaptureKeyboard)
         return;
 
-    if (event.key.keysym.sym == SDLK_ESCAPE)
-        handle_event(Event::CloseWindow);
-    else if (event.key.keysym.sym == SDLK_F1)
-        handle_event(Event::ToggleHelp);
-    else if (event.key.keysym.sym == SDLK_SPACE)
-        handle_event(Event::PlaybackTogglePause);
-    else if (event.key.keysym.sym == SDLK_LEFT)
-        handle_event(Event::JumpBackwardShort);
-    else if (event.key.keysym.sym == SDLK_DOWN)
-        handle_event(Event::JumpBackwardLong);
-    else if (event.key.keysym.sym == SDLK_RIGHT)
-        handle_event(Event::JumpForwardShort);
-    else if (event.key.keysym.sym == SDLK_UP)
-        handle_event(Event::JumpForwardLong);
+    switch (event.key.keysym.sym) {
+        case SDLK_ESCAPE: handle_event(Event::CloseWindow); break;
+        case SDLK_SPACE: handle_event(Event::PlaybackTogglePause); break;
+        case SDLK_F1: handle_event(Event::ToggleHelp); break;
+        case SDLK_RIGHT: handle_event(Event::JumpForwardShort); break;
+        case SDLK_LEFT: handle_event(Event::JumpBackwardShort); break;
+        case SDLK_DOWN: handle_event(Event::JumpBackwardLong); break;
+        case SDLK_UP: handle_event(Event::JumpForwardLong); break;
+    }
 }
 
 EventHandler::EventHandler() : impl_(std::make_unique<EventHandler::Impl>()) { }
